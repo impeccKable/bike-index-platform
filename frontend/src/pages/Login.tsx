667441ba932db7bee1061f6e68c469cb2ef92212
ProceduralCode <37784785+ProceduralCode@users.noreply.github.com>
2023-05-12 18:14:51 -0700

    Textbox tests

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,37 @@
+import { useState } from 'react'
 import '../App.css'
 
 function Login() {
+  const [searchText, setSearchText] = useState("")
+  const [searchTextType, setSearchTextType] = useState("")
+
+  // function isEmail(text: string) {
+  //   return text.includes("@")
+  // }
+  // function isPhone(text: string) {
+  //   return text.match(/^[0-9 +\-\.\(\)]*[0-9]+[0-9 +\-\.\(\)]*$/)
+  // }
+
+  function handleSearchInput(e: any) {
+    let newSearchText = e.target.value
+    let newSearchTextType = ""
+    if (newSearchText.includes("@")) {
+      newSearchTextType = "email"
+    } else if (newSearchText.match(/^[0-9 +\-\.\(\)]*[0-9]+[0-9 +\-\.\(\)]*$/)) {
+      newSearchTextType = "phone"
+    } else {
+      newSearchTextType = ""
+    }
+    setSearchTextType(newSearchTextType)
+    setSearchText(newSearchText)
+  }
+
   return (
     <>
       <h1>Login Page</h1>
+      Username <input type="text" value={searchText} onChange={handleSearchInput}/>
+      <br></br>
+      {searchTextType}
     </>
   )
 }
