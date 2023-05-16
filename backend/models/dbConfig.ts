242456fe90376f060f716103e09ac668dfc5e73d
boalo <c.chlin@icloud.com>
2023-05-15 21:03:48 -0700

    converted to typescript

--- backend/models/dbConfig.ts
+++ backend/models/dbConfig.ts
@@ -1,24 +1,22 @@
 /** @format */
 
-// const mysql = require('mysql');
-// import mysql from 'mysql';
+import mysql from 'mysql';
 
 const useDb = false;
 
 let connection;
 
-// if (useDb) {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'dbuser',
-//     password: 'my_password',
-//     database: 'my_db',
-//   });
-// } else {
-//   connection = {
-//     /* use test db */
-//   };
-// }
+if (useDb) {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'dbuser',
+    password: 'my_password',
+    database: 'my_db',
+  });
+} else {
+  connection = {
+    /* use test db */
+  };
+}
 
-// module.exports = connection;
 export default connection;
