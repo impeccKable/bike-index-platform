1a0d09ff2995f6a56f23db544c94baca6fab2562
boalo <c.chlin@icloud.com>
2023-05-16 01:25:28 -0700

    search by link route created and linked to its controller

--- backend/routes/marketplace.ts
+++ backend/routes/marketplace.ts
@@ -4,5 +4,7 @@ import marketplaceController from '../controllers/marketplaceController';
 const router = express.Router();
 
 router.get('/', marketplaceController.getAll);
+router.get('/search', marketplaceController.getByLink);
+// router.post('/', marketplaceController.allOne);
 
 export default router;
