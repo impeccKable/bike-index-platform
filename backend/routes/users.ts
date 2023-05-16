063f5afc4c2c81a94a72016b25dd438bac9880c4
boalo <c.chlin@icloud.com>
2023-05-16 01:19:16 -0700

    minor change

--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -6,7 +6,7 @@ import usersController from '../controllers/usersController';
 const router = express.Router();
 
 router.get('/', usersController.getAll); //no db yet
-router.get('/:id(\\d+)', usersController.getById); // no db yet
+router.get('/search', usersController.getById); // no db yet
 router.get('/fake', usersController.getAllFake);
 router.post('/', usersController.addFake);
 
