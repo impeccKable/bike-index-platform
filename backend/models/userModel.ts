d460662a8d5e96691f8c8d508d7e31d0a80b495a
boalo <c.chlin@icloud.com>
2023-05-15 21:31:21 -0700

    callback function type specified

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -1,19 +1,18 @@
-// /** @format */
+/** @format */
 
-// // const db = require('./dbConfig');
-// import db from './dbConfig';
+import db from './dbConfig';
+import { queryCallback } from 'mysql';
 
-// const userModel = {
-//   getAll: (theCallbackFromController: any) => {
-//     const query = 'SELECT * FROM userTable';
-//     db.query(query, theCallbackFromController);
-//   },
+const userModel = {
+  getAll: (theCallbackFromController: queryCallback) => {
+    const query = 'SELECT * FROM userTable';
+    db.query(query, theCallbackFromController);
+  },
 
-//   getById: (id: number, callback: any) => {
-//     const query = 'SELECT * FROM userTable WHERE id = ?';
-//     db.query(query, [id], callback);
-//   },
-// };
+  getById: (id: number, callback: queryCallback) => {
+    const query = 'SELECT * FROM userTable WHERE id = ?';
+    db.query(query, [id], callback);
+  },
+};
 
-// // module.exports = userModel;
-// export default userModel;
+export default userModel;
