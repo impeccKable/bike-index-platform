f7f19f2207b4572c8ad58d19e12b5855f6f05c9b
boalo <c.chlin@icloud.com>
2023-05-16 01:26:27 -0700

    marketplace link controller's getByLink created' '

--- backend/controllers/marketplaceController.ts
+++ backend/controllers/marketplaceController.ts
@@ -9,6 +9,19 @@ const marketplaceController = {
       res.status(500).send('Error getting all from db');
     }
   },
+
+  getByLink: (req: Request, res: Response) => {
+    const { link } = req.query;
+    if (typeof link === 'string') {
+      try {
+        res.json(marketplaceModel.getByLink(link));
+      } catch (err) {
+        res.status(500).send('Err getting link data from db');
+      }
+    } else {
+      res.status(400).send('Invalid link parameter');
+    }
+  },
 };
 
 export default marketplaceController;
