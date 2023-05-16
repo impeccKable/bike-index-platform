e4b17ddf325518e41d3cf168c89cf9449980f1e1
boalo <c.chlin@icloud.com>
2023-05-15 21:40:29 -0700

    Remove dist folder from tracking

--- backend/dist/controllers/indexController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/** @format */
-Object.defineProperty(exports, "__esModule", { value: true });
-const indexController = {
-    home: (req, res) => {
-        res.send('home page');
-    },
-};
-exports.default = indexController;
