d0b413d15f9a5f973f184e21ff74b2b7bca3990f
boalo <c.chlin@icloud.com>
2023-05-16 00:04:07 -0700

    connected to marketplaceModel

--- backend/controllers/marketplaceController.ts
+++ backend/controllers/marketplaceController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express';
-// import marketplaceModel from '../models/marketplaceModel';
+import marketplaceModel from '../models/marketplaceModel';
 
 const marketplaceController = {
   getAll: (req: Request, res: Response) => {
     try {
-      res.send('dummy get all');
+      res.json(marketplaceModel.getAll());
     } catch (err) {
       res.status(500).send('Error getting all from db');
     }
