4d54206b598c615bdb0306313897946af59ca3ca
boalo <c.chlin@icloud.com>
2023-05-15 21:59:09 -0700

    route to marketplace page added

--- backend/app.ts
+++ backend/app.ts
@@ -3,6 +3,7 @@
 import express from 'express';
 import index from './routes/index';
 import name from './routes/name';
+import marketplace from './routes/marketplace';
 
 const port = process.env.PORT || 3000;
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use('/', index);
 app.use('/name', name);
+app.use('/marketplace', marketplace);
 
 app.listen(port, () => {
   console.log(`listening on port http://localhost:${port}`);
