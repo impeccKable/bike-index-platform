0df60eb19470eb620b946d8e03f31f78ca531745
m-istvan <56458289+m-istvan@users.noreply.github.com>
2023-05-20 16:43:48 -0700

    User routes/controller/model faked out implement

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -1,4 +1,44 @@
 /** @format */
+import { faker } from "@faker-js/faker";
+
+const fakeUser: { id: number; User: string; Password: string }[] = [];
+for (let i = 0; i <= 20; i++) {
+	const newUser = {
+		id: i + 1,
+		User: faker.internet.email(),
+		Password: faker.internet.password(),
+	};
+	fakeUser.push(newUser);
+}
+
+const userModel = {
+	getAll: () => {
+		return fakeUser;
+	},
+
+	getByUserEmail: (UserEmail: string) => {
+		const result: { id: number; User: string; Password: string }[] = [];
+		fakeUser.forEach((UserEntry) => {
+			if (UserEntry.User === UserEmail) {
+				result.push(UserEntry);
+			}
+		});
+		return result;
+	},
+
+	addOne: (UserEmail: string, UserPass: string) => {
+		try {
+			const newUser = {
+				id: fakeUser.length + 1,
+				User: UserEmail,
+				Password: UserPass,
+			};
+			fakeUser.push(newUser);
+		} catch (err) {
+			return err;
+		}
+	},
+};
 
 /* import db from './dbConfig';
 import { queryCallback } from 'mysql';
@@ -15,5 +55,4 @@ const userModel = {
   },
 };
  */
-const userModel = {};
 export default userModel;
--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -1,13 +1,12 @@
 /** @format */
 
-import express from 'express';
-import usersController from '../controllers/usersController';
+import express from "express";
+import usersController from "../controllers/usersController";
 
 const router = express.Router();
 
-router.get('/', usersController.getAll); //no db yet
-router.get('/search', usersController.getById); // no db yet
-router.get('/fake', usersController.getAllFake);
-router.post('/', usersController.addFake);
+router.get("/", usersController.getAll); //no db yet
+router.get("/search", usersController.getByEmail); // no db yet
+router.post("/", usersController.addFake);
 
 export default router;
