00f9c9bcda7741c74f5084db258a1cc3a14cd8f2
boalo <c.chlin@icloud.com>
2023-07-10 18:25:35 -0700

    fixed URI malform

--- frontend/src/services/HttpClient.tsx
+++ frontend/src/services/HttpClient.tsx
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
 const env = import.meta.env;
 
 const httpClient = axios.create({
-	baseURL: `http://${env.VITE_APIHOST}:${env.VITE_APIPORT}`,
-	headers: {
-		"Content-type": "application/json",
-	},
+  baseURL: `http://${env.VITE_APIHOST}:${env.VITE_PORT}`,
+  headers: {
+    'Content-type': 'application/json',
+  },
 });
 
 export { httpClient };
