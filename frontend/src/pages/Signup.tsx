7f318cfec26a15ed8a5de5fee2758f656b6d77a8
boalo <c.chlin@icloud.com>
2023-07-10 18:26:00 -0700

    delete firebase user if POST to backend fails

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -4,58 +4,80 @@ import { useAuth } from '../services/AuthProvider';
 import { httpClient } from '../services/HttpClient';
 
 export default function Signup() {
-	const [submitted, setSubmitted] = useState(false);
-	const [passwordMismatch, setPasswordMismatch] = useState(false);
-	const auth = useAuth();
+  const [submitted, setSubmitted] = useState(false);
+  const [passwordMismatch, setPasswordMismatch] = useState(false);
+  const auth = useAuth();
 
-	function handleFormSubmit(e: any) {
-		e.preventDefault();
-		if (submitted) { return }
-		if (e.dataDict.password !== e.dataDict.verify) {
-			setPasswordMismatch(true);
-			return;
-		}
+  function handleFormSubmit(e: any) {
+    e.preventDefault();
+    if (submitted) {
+      return;
+    }
+    if (e.dataDict.password !== e.dataDict.verify) {
+      setPasswordMismatch(true);
+      return;
+    }
 
-		const f = async () => {
-			const uid = await auth?.handleSignUp(e.dataDict.email, e.dataDict.password);
-			console.log(uid);
-			if (uid) {
-				e.dataDict.uid = uid;
-				delete e.dataDict.password;
-				delete e.dataDict.verify;
-				console.log(e.dataDict);
-				httpClient.post('/signup', e.dataDict)
-					.then((res: any) => {
-						console.log(res);
-					})
-					.catch((err: any) => {
-						console.log(err);
-					});
-			}
-		};
-		f();
+    const f = async () => {
+      const user = await auth?.handleSignUp(
+        e.dataDict.email,
+        e.dataDict.password
+      );
+      console.log(user.uid);
+      if (user.uid) {
+        e.dataDict.uid = user.uid;
+        delete e.dataDict.password;
+        delete e.dataDict.verify;
+        console.log(e.dataDict);
+        await httpClient
+          .post('/signup', e.dataDict)
+          .then((res: any) => {
+            console.log(res);
+          })
+          .catch((err: any) => {
+            console.log(err);
+            auth?.handleDelete(user);
+          });
+      }
+    };
+    f();
 
-		setSubmitted(true);
-	}
-	let submitMessage = (
-		<div className="submit-message">
-			<p>Your request to sign up has been submitted!</p>
-			<p>We will review your application and get back to you.</p>
-		</div>
-	);
-	return (
-		<div className="signup-page">
-			<h1 className="title">Sign Up</h1>
-			<Form onSubmit={handleFormSubmit}>
-				<FormInput label="First name"    name="first"    placeholder="John" />
-				<FormInput label="Last name"     name="last"     placeholder="Doe" />
-				<FormInput label="Title"         name="title"    placeholder="Mayor" />
-				<FormInput label="Organization"  name="org" />
-				<FormInput label="Email"         name="email"    placeholder="email@example.com" type="email" />
-				<FormInput label="Password"      name="password" type="password" />
-				<FormInput label="(again)"       name="verify"   type="password" labelProps={{ style: { color: '#666' }}} />
-				<FormInput label="Phone"         name="phone"    placeholder="+1 222 333 4444" type="phone" />
-				{/*
+    setSubmitted(true);
+  }
+  let submitMessage = (
+    <div className="submit-message">
+      <p>Your request to sign up has been submitted!</p>
+      <p>We will review your application and get back to you.</p>
+    </div>
+  );
+  return (
+    <div className="signup-page">
+      <h1 className="title">Sign Up</h1>
+      <Form onSubmit={handleFormSubmit}>
+        <FormInput label="First name" name="first" placeholder="John" />
+        <FormInput label="Last name" name="last" placeholder="Doe" />
+        <FormInput label="Title" name="title" placeholder="Mayor" />
+        <FormInput label="Organization" name="org" />
+        <FormInput
+          label="Email"
+          name="email"
+          placeholder="email@example.com"
+          type="email"
+        />
+        <FormInput label="Password" name="password" type="password" />
+        <FormInput
+          label="(again)"
+          name="verify"
+          type="password"
+          labelProps={{ style: { color: '#666' } }}
+        />
+        <FormInput
+          label="Phone"
+          name="phone"
+          placeholder="+1 222 333 4444"
+          type="phone"
+        />
+        {/*
 				TODO: change this back to required
 				<FormInput label="First name" name="first-name" required placeholder="John" />
 				<FormInput label="Last name"  name="last-name"  required placeholder="Doe" />
@@ -64,17 +86,17 @@ export default function Signup() {
 				<FormInput label="Password"   name="password"   required type="password" />
 				<FormInput label="(again)"    name="verify"     required type="password" labelProps={{ style: { color: '#666' }}} />
 				<FormInput label="Phone"      name="phone"      required placeholder="+1 222 333 4444" type="phone" /> */}
-				<div className="links">
-					<LinkButton to="..">Back</LinkButton>
-					<FormButton type="submit">Submit</FormButton>
-				</div>
-				{submitted && submitMessage}
-				{passwordMismatch && (
-					<div>
-						<p>Passwords did not match, please try again</p>
-					</div>
-				)}
-			</Form>
-		</div>
-	);
+        <div className="links">
+          <LinkButton to="..">Back</LinkButton>
+          <FormButton type="submit">Submit</FormButton>
+        </div>
+        {submitted && submitMessage}
+        {passwordMismatch && (
+          <div>
+            <p>Passwords did not match, please try again</p>
+          </div>
+        )}
+      </Form>
+    </div>
+  );
 }
--- frontend/src/services/AuthProvider.tsx
+++ frontend/src/services/AuthProvider.tsx
@@ -1,140 +1,152 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import {
-	getAuth,
-	signInWithEmailAndPassword,
-	signOut,
-	onAuthStateChanged,
-	UserCredential,
-	User,
-	createUserWithEmailAndPassword,
-} from "firebase/auth";
-import React, { useState, useContext, useEffect } from "react";
-import { httpClient } from "./HttpClient";
-import { useRecoilValue } from "recoil";
-import { debugState } from "../services/Recoil";
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  UserCredential,
+  User,
+  createUserWithEmailAndPassword,
+  deleteUser,
+} from 'firebase/auth';
+import React, { useState, useContext, useEffect } from 'react';
+import { httpClient } from './HttpClient';
+import { useRecoilValue } from 'recoil';
+import { debugState } from '../services/Recoil';
 
 const AuthContext = React.createContext<AuthContextProps | null>(null);
 
 const app = initializeApp({
-	apiKey:            import.meta.env.VITE_APIKEY,
-	authDomain:        import.meta.env.VITE_AUTHDOMAIN,
-	projectId:         import.meta.env.VITE_PROJECTID,
-	storageBucket:     import.meta.env.VITE_STORAGEBUCKET,
-	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-	appId:             import.meta.env.VITE_APPID,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 });
 
 export const auth = getAuth(app);
 
 export type UserInfo = {
-	firebase: User | null;
-	bikeIndex: any | null; //create type for this later
+  firebase: User | null;
+  bikeIndex: any | null; //create type for this later
 };
 
 export type AuthContextProps = {
-	user: UserInfo | null;
-	handleLogin: (email: string, password: string) => Promise<Boolean>;
-	handleLogout: () => void;
-	handleSignUp: (email: string, password: string) => Promise<string>;
+  user: UserInfo | null;
+  handleLogin: (email: string, password: string) => Promise<Boolean>;
+  handleLogout: () => void;
+  handleSignUp: (email: string, password: string) => Promise<string>;
+  handleDelete: (user: User) => void;
 };
 
 export const AuthProvider = ({ children }: any) => {
-	if (useRecoilValue(debugState) == true) {
-		console.log("AuthProvider");
-	}
+  if (useRecoilValue(debugState) == true) {
+    console.log('AuthProvider');
+  }
 
-	const retrieveUser = () => {
-		return JSON.parse(localStorage.getItem("user") ?? "null");
-	}
-	const [user, setUser] = useState<UserInfo | null>(retrieveUser());
+  const retrieveUser = () => {
+    return JSON.parse(localStorage.getItem('user') ?? 'null');
+  };
+  const [user, setUser] = useState<UserInfo | null>(retrieveUser());
 
-	const handleLogin = async (email: string, password: string) => {
-		try {
-			const login = await signInWithEmailAndPassword(
-				auth,
-				email,
-				password
-				);
-			const user = {
-				firebase: login.user,
-				bikeIndex: (await httpClient.post("/login", { uid: login.user.uid })).data,
-			}
-			if(user.bikeIndex.banned === true) {
-				throw new Error("User is banned");
-			}
-			else if(user.bikeIndex.approved === false) {
-				throw new Error("User is not verified");
-			}
-			await updateAxios(await user.firebase.getIdToken());
-			updateUser(user);
-			return true;
-		} catch (err) {
-			console.error(err);
-		}
-		return false;
-	};
+  const handleLogin = async (email: string, password: string) => {
+    try {
+      const login = await signInWithEmailAndPassword(auth, email, password);
+      const user = {
+        firebase: login.user,
+        bikeIndex: (await httpClient.post('/login', { uid: login.user.uid }))
+          .data,
+      };
+      if (user.bikeIndex.banned === true) {
+        throw new Error('User is banned');
+      } else if (user.bikeIndex.approved === false) {
+        throw new Error('User is not verified');
+      }
+      await updateAxios(await user.firebase.getIdToken());
+      updateUser(user);
+      return true;
+    } catch (err) {
+      console.error(err);
+    }
+    return false;
+  };
 
-	const handleLogout = () => {
-		updateUser(null);
-		signOut(auth);
-	};
+  const handleLogout = () => {
+    updateUser(null);
+    signOut(auth);
+  };
 
-	const handleSignUp = async (email: string, password: string) => {
-		try {
-			let userData = await createUserWithEmailAndPassword(auth, email, password);
-			return userData.user.uid;
-		} catch (err) {
-			console.error(err);
-		}
-		return '';
-	};
-	const updateAxios = async (token: string) => {
-		console.log(`token: ...${token.slice(-10)}`);
-		httpClient.interceptors.request.use(
-			async (config) => {
-				// @ts-ignore
-				config.headers = {
-					Authorization: `Bearer ${token}`,
-					Accept: "application/json",
-				};
+  const handleSignUp = async (email: string, password: string) => {
+    try {
+      let userData = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      return userData.user;
+    } catch (err) {
+      console.error(err);
+    }
+    return '';
+  };
+  const updateAxios = async (token: string) => {
+    console.log(`token: ...${token.slice(-10)}`);
+    httpClient.interceptors.request.use(
+      async (config) => {
+        // @ts-ignore
+        config.headers = {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/json',
+        };
 
-				return config;
-			},
-			(error) => {
-				console.error("REJECTED TOKEN PROMISE");
-				Promise.reject(error);
-			}
-		);
-	};
+        return config;
+      },
+      (error) => {
+        console.error('REJECTED TOKEN PROMISE');
+        Promise.reject(error);
+      }
+    );
+  };
 
-	const updateUser = (newUser: UserInfo | null) => {
-		localStorage.setItem("user", JSON.stringify(newUser));
-		setUser(newUser);
-	}
-	useEffect(() => {
-		const prevUser = retrieveUser();
-		if (prevUser) {
-			updateAxios(prevUser.firebase.stsTokenManager.accessToken);
-		}
-		setUser(prevUser);
-	}, []);
+  const updateUser = (newUser: UserInfo | null) => {
+    localStorage.setItem('user', JSON.stringify(newUser));
+    setUser(newUser);
+  };
 
+  const handleDelete = async (user: User) => {
+    try {
+      const uid = user.uid;
+      await deleteUser(user);
+      console.log(`User ${uid} deleted`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	return (
-		<AuthContext.Provider
-			value={{
-				user,
-				handleLogin,
-				handleLogout,
-				handleSignUp,
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
+  useEffect(() => {
+    const prevUser = retrieveUser();
+    if (prevUser) {
+      updateAxios(prevUser.firebase.stsTokenManager.accessToken);
+    }
+    setUser(prevUser);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        handleLogin,
+        handleLogout,
+        handleSignUp,
+        handleDelete,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = (): any => {
-	return useContext(AuthContext);
+  return useContext(AuthContext);
 };
-
