0193ad11cd03d85db54eeb77d1074fb6f9e6819c
Cheng Lin <c.chlin@icloud.com>
2023-07-27 04:52:23 -0700

    inital display for uploaded file

--- frontend/src/components/Gallery.tsx
+++ frontend/src/components/Gallery.tsx
@@ -1,8 +1,22 @@
+import { useEffect, useRef } from 'react';
+
 interface GalleryProps {
   files: (string | File)[];
 }
 
 export function Gallery({ files }: GalleryProps) {
+  const createdUrlsRef = useRef<(string | null)[]>([]);
+
+  useEffect(() => {
+    return () => {
+      createdUrlsRef.current.forEach((url) => {
+        if (url) {
+          URL.revokeObjectURL(url);
+        }
+      });
+    };
+  }, [files]);
+
   return (
     <div className="gallery">
       {files.map((item, index) => {
@@ -10,6 +24,7 @@ export function Gallery({ files }: GalleryProps) {
           return <img key={index} src={item} alt={`Image ${index}`} />;
         } else {
           const createdUrl = URL.createObjectURL(item);
+          createdUrlsRef.current[index] = createdUrl;
           return <img key={index} src={createdUrl} alt={`Uploaded ${item.name}`} />;
         }
       })}
