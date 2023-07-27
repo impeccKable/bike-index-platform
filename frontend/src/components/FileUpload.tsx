7704331885bcde93539357c41b4a1725859051e6
Cheng Lin <c.chlin@icloud.com>
2023-07-27 03:16:51 -0700

    upload file modal select file file type and size guarded at the frontend

--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -6,6 +6,26 @@ interface FileUploadProps {
 
 export function FileUpload(props: FileUploadProps) {
 	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+	const [errorMessage, setErrorMessage] = useState<string | null>(null);
+	const maxSize = 1024 * 1024 * 25;
+	const fileTypes = ['image/jpeg', 'image/jpg', 'image/png']
+
+	function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+		setUploadedFile(null);
+		setErrorMessage(null);
+		if (event.target.files) {
+			const file = event.target.files[0];
+
+			if (!fileTypes.includes(file.type)) {
+				setErrorMessage("Invalid file type");
+			} else if (!(file.size <= maxSize)) {
+				setErrorMessage("File size is too large");
+			} else {
+				setUploadedFile(file);
+			}
+		}
+	}
 
 	return (
 		<>
@@ -17,8 +37,10 @@ export function FileUpload(props: FileUploadProps) {
 			{isModalOpen && (
 				<div className="modal">
 					<div className="modal-content">
-						<span className="close" onClick={() => setIsModalOpen(false)}>&times;</span>
-						<input type="file" />
+						<span className="modal-btn close" onClick={() => setIsModalOpen(false)}>&#215;</span>
+						<input type="file" className="file-name" onChange={handleFileChange} />
+						<span className="error-message">{errorMessage}</span>
+						<button className="confirm" disabled={!!errorMessage || !uploadedFile}>Upload</button>
 					</div>
 				</div>
 			)}
