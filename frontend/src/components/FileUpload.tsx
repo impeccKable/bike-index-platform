3a3fae2389906e05be16b40723c60cf8b1c86cf3
Cheng Lin <c.chlin@icloud.com>
2023-07-27 01:27:57 -0700

    add button modal for upload

--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
+
 interface FileUploadProps {
 	label: string;
 }
 
 export function FileUpload(props: FileUploadProps) {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
 	return (
 		<>
 			<label>{props.label}</label>
-			<button className="file-upload-btn" type="button">
+			<button className="file-upload-btn" type="button" onClick={() => setIsModalOpen(true)}>
 				＋
 			</button>
+
+			{isModalOpen && (
+				<div className="modal">
+					<div className="modal-content">
+						<span className="close" onClick={() => setIsModalOpen(false)}>&times;</span>
+						<input type="file" />
+					</div>
+				</div>
+			)}
 		</>
 	);
 }
