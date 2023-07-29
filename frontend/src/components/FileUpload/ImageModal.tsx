80207ca67d34881b484ff3aa25d9faecbc9b04dd
Cheng Lin <c.chlin@icloud.com>
2023-07-29 00:38:31 -0700

    fixed the file upload button hover color; close modal when user click anywhere other than the modal

--- frontend/src/components/FileUpload/ImageModal.tsx
+++ frontend/src/components/FileUpload/ImageModal.tsx
@@ -1,48 +1,41 @@
 interface ImageModalProps {
-	imageUrl: string;
-	index: number;
-	handleClose: () => void;
-	handleNext: (index: number) => void;
-	handlePrev: (index: number) => void;
-	// handleDelete: () => void;
+  imageUrl: string;
+  index: number;
+  handleClose: () => void;
+  handleNext: (index: number) => void;
+  handlePrev: (index: number) => void;
+  // handleDelete: () => void;
 }
 
 export function ImageModal(props: ImageModalProps) {
-	function handleClickImage() {
-		window.open(props.imageUrl, '_blank');
-	}
 
-	function handleOnClickNext(event: React.MouseEvent) {
-		event.stopPropagation();
-		props.handleNext(props.index);
-	}
+  function handleClickImage() {
+    window.open(props.imageUrl, "_blank");
+  }
 
-	function handleOnClickPrev(event: React.MouseEvent) {
-		event.stopPropagation();
-		props.handlePrev(props.index);
-	}
-	return (
-		<div className="image-modal">
-			<div className="modal">
-				<div className="modal-content">
-					<span className="modal-btn close" onClick={props.handleClose}>
-						&#215;
-					</span>
-					<img
-						src={props.imageUrl}
-						alt={`Image $eprops.index}`}
-						onClick={handleClickImage}
-					/>
-				</div>
-			</div>
-			<div className="image-modal-btns">
-				<span className="modal-btn prev" onClick={handleOnClickPrev}>
-					&lt;
-				</span>
-				<span className="modal-btn next" onClick={handleOnClickNext}>
-					&gt;
-				</span>
-			</div>
-		</div>
-	);
+  function handleOnClickNext(event: React.MouseEvent) {
+    // event.stopPropagation();
+    props.handleNext(props.index);
+  }
+  
+  function handleOnClickPrev(event: React.MouseEvent) {
+    // event.stopPropagation();
+    props.handlePrev(props.index);
+  }
+
+  return (
+    <div className="image-modal">
+      <div className="modal" onClick={props.handleClose}>
+        <div className="modal-content">
+          <span className="modal-btn close" onClick={props.handleClose}>&#215;</span>
+          <img src={props.imageUrl} alt={`Image $eprops.index}`} onClick={handleClickImage} />
+        </div>
+      </div>
+        <div className="image-modal-btns">
+          <span className="modal-btn prev" onClick={handleOnClickPrev}>&lt;</span>
+          <span className="modal-btn next" onClick={handleOnClickNext}>&gt;</span>
+        </div>
+    </div>
+  )
 }
+
