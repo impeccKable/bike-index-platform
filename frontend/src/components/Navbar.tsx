87580d6f0c42816f3f2832042030d3f95abe7c94
Cristian Torres Salazar <tor8@pdx.edu>
2023-07-29 00:29:31 -0700

    Initial Changes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
 		<nav>
 			<LinkButton className="btn-home" to="/">Home</LinkButton>
 			<LinkButton to="/thiefList">Thief List</LinkButton>
-			<LinkButton to=".">User List</LinkButton>
+			<LinkButton to="/userList">User List</LinkButton>
 			<LinkButton to="/import">Data</LinkButton>
 			<LinkButton to="/stats">Stats</LinkButton>
 			<LinkButton to="/about">About</LinkButton>
---
+++ frontend/src/pages/UserList.tsx
@@ -0,0 +1,76 @@
+
+// Components
+import Navbar from "../components/Navbar";
+import LinkTable from "../components/LinkTable";
+import { LinkButton } from "../components/Form";
+import { httpClient } from "../services/HttpClient";
+
+// react
+import {useState, useEffect, useRef} from 'react';
+
+// headers: ID, Name (First / Last), email, phone, role
+const tableHeaders = {
+	'ID':      { maxWidth: "2rem",  minWidth: "2rem" },
+	'Name':    { maxWidth: "13rem", minWidth: "13rem"},
+	'Phone':   { maxWidth: "6rem",  minWidth: "6rem" },
+	'Email':   { maxWidth: "14rem", minWidth: "14rem"},
+	'role':    { maxWidth: "15rem", minWidth: "15rem"},
+};
+
+export default function UserList() {
+    const [searchType, setSearchType] = useState('name');
+    const [searchText, setSearchText] = useState("");
+    const preSearchText = useRef(searchText);
+
+    useEffect(() => {
+
+
+        const GetUsers = async () => {
+            preSearchText.current = searchText;
+
+            const response = await httpClient.get("")
+            .catch((err: any) => {
+                // DebugLogs
+            });
+
+        }
+
+
+    }, []);
+
+    return (
+        <div className="formal">
+            <Navbar/>
+            <main>
+            <h1>User Listing</h1>
+            <div className="searchbar">
+                <label htmlFor="SearchType">Search Type</label>
+                <select 
+                id="SearchType"
+                name="SearchType"
+                onChange={(event: any) => {
+                    setSearchType(event.target[event.target.selectedIndex].value);
+                }}
+                >
+                    <option value="name">Name</option>
+                    <option value="phone">Phone Number</option>
+                    <option value="email">Email</option>
+                </select>
+                <label htmlFor="UserSearch">Search</label>
+                <input
+                    id="UserSearch"
+                    type={searchType}
+                    required
+                    value={searchText}
+                    onChange={(event: any) => {
+                        setSearchText(event.target.value);
+                    }}
+                    >
+                </input>
+                <LinkButton className="AddThiefButton" to="">Add New</LinkButton>
+            </div>
+            <LinkTable header={tableHeaders} linkBase=""></LinkTable>
+            </main>
+        </div>
+    );
+}
