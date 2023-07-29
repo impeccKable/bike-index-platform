2649c282cb5bfa94f5595ed6a7ce4e6d45bc25fc
Cheng Lin <c.chlin@icloud.com>
2023-07-28 21:01:02 -0700

    styled ImageModal and user will be able to view the full image in a new tab

--- frontend/src/components/FileUpload/ImageModal.tsx
+++ frontend/src/components/FileUpload/ImageModal.tsx
@@ -8,12 +8,17 @@ interface ImageModalProps {
 }
 
 export function ImageModal(props: ImageModalProps) {
+
+  function handleClickImage() {
+    window.open(props.imageUrl, "_blank");
+  }
+
   return (
     <div className="image-modal">
       <div className="modal">
         <div className="modal-content">
-          <img src={props.imageUrl} alt={`Image $eprops.index}`} />
           <span className="modal-btn close" onClick={props.handleClose}>&#215;</span>
+          <img src={props.imageUrl} alt={`Image $eprops.index}`} onClick={handleClickImage} />
         </div>
       </div>
     </div>
