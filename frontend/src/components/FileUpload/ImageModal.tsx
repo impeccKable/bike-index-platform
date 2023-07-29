461bcbc003e4b58005726a2ebe66fa291e324798
Cheng Lin <c.chlin@icloud.com>
2023-07-28 23:56:33 -0700

    foramt the space to tab

--- frontend/src/components/FileUpload/ImageModal.tsx
+++ frontend/src/components/FileUpload/ImageModal.tsx
@@ -1,40 +1,48 @@
 interface ImageModalProps {
-  imageUrl: string;
-  index: number;
-  handleClose: () => void;
-  handleNext: (index: number) => void;
-  handlePrev: (index: number) => void;
-  // handleDelete: () => void;
+	imageUrl: string;
+	index: number;
+	handleClose: () => void;
+	handleNext: (index: number) => void;
+	handlePrev: (index: number) => void;
+	// handleDelete: () => void;
 }
 
 export function ImageModal(props: ImageModalProps) {
+	function handleClickImage() {
+		window.open(props.imageUrl, '_blank');
+	}
 
-  function handleClickImage() {
-    window.open(props.imageUrl, "_blank");
-  }
+	function handleOnClickNext(event: React.MouseEvent) {
+		event.stopPropagation();
+		props.handleNext(props.index);
+	}
 
-  function handleOnClickNext(event: React.MouseEvent) {
-    event.stopPropagation();
-    props.handleNext(props.index);
-  }
-  
-  function handleOnClickPrev(event: React.MouseEvent) {
-    event.stopPropagation();
-    props.handlePrev(props.index);
-  }
-
-  return (
-    <div className="image-modal">
-      <div className="modal">
-        <div className="modal-content">
-          <span className="modal-btn close" onClick={props.handleClose}>&#215;</span>
-          <div className="image-modal-btns">
-            <span className="modal-btn prev" onClick={handleOnClickPrev}>&lt;</span>
-            <span className="modal-btn next" onClick={handleOnClickNext}>&gt;</span>
-          </div>
-          <img src={props.imageUrl} alt={`Image $eprops.index}`} onClick={handleClickImage} />
-        </div>
-      </div>
-    </div>
-  )
+	function handleOnClickPrev(event: React.MouseEvent) {
+		event.stopPropagation();
+		props.handlePrev(props.index);
+	}
+	return (
+		<div className="image-modal">
+			<div className="modal">
+				<div className="modal-content">
+					<span className="modal-btn close" onClick={props.handleClose}>
+						&#215;
+					</span>
+					<img
+						src={props.imageUrl}
+						alt={`Image $eprops.index}`}
+						onClick={handleClickImage}
+					/>
+				</div>
+			</div>
+			<div className="image-modal-btns">
+				<span className="modal-btn prev" onClick={handleOnClickPrev}>
+					&lt;
+				</span>
+				<span className="modal-btn next" onClick={handleOnClickNext}>
+					&gt;
+				</span>
+			</div>
+		</div>
+	);
 }
--- frontend/src/components/FileUpload/Thumbnail.tsx
+++ frontend/src/components/FileUpload/Thumbnail.tsx
@@ -2,46 +2,58 @@ import { useState, useEffect } from 'react';
 import { ImageModal } from './ImageModal';
 
 interface ThumbnailProps {
-  file: string | File;
-  index: number;
-  handleNext: (index: number) => void;
-  handlePrev: (index: number) => void;
-  currentViewing: string | File | null;
-  setCurrentViewing: (file: string | File | null) => void;
+	file: string | File;
+	index: number;
+	handleNext: (index: number) => void;
+	handlePrev: (index: number) => void;
+	currentViewing: string | File | null;
+	setCurrentViewing: (file: string | File | null) => void;
 }
 
 export function Thumbnail(props: ThumbnailProps) {
-  const [imageUrl, setImageUrl] = useState("");
-  // const [isModalOpen, setIsModalOpen] = useState(false);
+	const [imageUrl, setImageUrl] = useState('');
+	// const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    if (typeof props.file === 'string') {
-      setImageUrl(props.file);
-    } else {
-      const createdUrl = URL.createObjectURL(props.file);
-      setImageUrl(createdUrl);
+	useEffect(() => {
+		if (typeof props.file === 'string') {
+			setImageUrl(props.file);
+		} else {
+			const createdUrl = URL.createObjectURL(props.file);
+			setImageUrl(createdUrl);
 
-      return () => {
-        URL.revokeObjectURL(createdUrl);
-      };
-    }
-  }, [props.file]);
+			return () => {
+				URL.revokeObjectURL(createdUrl);
+			};
+		}
+	}, [props.file]);
 
-  function handleImageClick() {
-    // setIsModalOpen(true);
-    props.setCurrentViewing(props.file);
-  }
+	function handleImageClick() {
+		// setIsModalOpen(true);
+		props.setCurrentViewing(props.file);
+	}
 
-  function handleCloseModal() {
-    // setIsModalOpen(false);
-    props.setCurrentViewing(null);
-  }
+	function handleCloseModal() {
+		// setIsModalOpen(false);
+		props.setCurrentViewing(null);
+	}
 
-  return (
-    <div className="thumbnail">
-      <img key={props.index} src={imageUrl} alt={`Thumbnail ${props.index}`} onClick={handleImageClick} />
-      {props.currentViewing === props.file && <ImageModal imageUrl={imageUrl} index={props.index} handleClose={handleCloseModal} handleNext={() => props.handleNext(props.index)} 
-        handlePrev={() => props.handlePrev(props.index)} />}
-    </div>
-  )
+	return (
+		<div className="thumbnail">
+			<img
+				key={props.index}
+				src={imageUrl}
+				alt={`Thumbnail ${props.index}`}
+				onClick={handleImageClick}
+			/>
+			{props.currentViewing === props.file && (
+				<ImageModal
+					imageUrl={imageUrl}
+					index={props.index}
+					handleClose={handleCloseModal}
+					handleNext={() => props.handleNext(props.index)}
+					handlePrev={() => props.handlePrev(props.index)}
+				/>
+			)}
+		</div>
+	);
 }
