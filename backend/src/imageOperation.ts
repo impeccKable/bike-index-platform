dcf9c68a8a09ad8ee63f621cf2c2837046bf7cde
Cheng Lin <c.chlin@icloud.com>
2023-07-30 21:59:21 -0700

    process file name to generate unique name

--- backend/src/imageOperation.ts
+++ backend/src/imageOperation.ts
@@ -1,10 +1,14 @@
 import multer from 'multer';
+import path from 'path';
+import crypto from 'crypto';
 
-export const imageUpload = (uploadedFiles?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] }) => {
-	if (uploadedFiles) {
-		for (const file of [uploadedFiles]) {
-			console.log("Uploaded file", file);
-		}
+export const imageUpload = (uploadedFiles: Express.Multer.File[]) => {
+	let filename: string;
+	for (const file of uploadedFiles) {
+		filename = generateUniqueFilename(file.originalname);
+		console.log(filename);
+		// console.log("mimetype", file.mimetype);
+		// console.log("buffer", file.buffer);
 	}
 }
 
@@ -12,4 +16,13 @@ export const imageDelete = (deletedFile: string[]) => {
 	for (const file of deletedFile) {
 		console.log("Deleted file", file);
 	}
+}
+
+const generateUniqueFilename = (filename: string): string => {
+	const extension = path.extname(filename);
+	const basename = path.basename(filename, extension).replace(/\s/g, '-');
+
+	const randomString = crypto.randomBytes(5).toString('hex');
+
+	return `${basename}-${randomString}${extension}`;
 }
