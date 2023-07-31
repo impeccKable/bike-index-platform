913f93ad4caa4e2074f66655bac38ed9b2da4a90
Cheng Lin <c.chlin@icloud.com>
2023-07-30 23:46:16 -0700

    added environment variables for accessing s3

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,13 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { DB_USER, DB_PASSWORD } = process.env;
+const { DB_USER, DB_PASSWORD, BUCKET_NAME } = process.env;
 
-if (DB_USER === undefined || DB_PASSWORD === undefined) {
+if (DB_USER === undefined || DB_PASSWORD === undefined || BUCKET_NAME === undefined) {
 	throw new Error('Missing necessary environment variables');
 }
 
 export const config = {
 	dbUser: DB_USER,
 	dbPassword: DB_PASSWORD,
+	bucketName: BUCKET_NAME,
 };
