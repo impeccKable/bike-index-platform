49c4eed95f41d25d0838eddfdaf49010a70d03e4
Cristian Torres Salazar <tor8@pdx.edu>
2023-08-01 14:37:43 -0700

    Fixed Navbar after merge

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,8 +4,8 @@ function Navbar() {
 	return (
 		<nav>
 			<LinkButton className="btn-home" to="/">Home</LinkButton>
-			<LinkButton to="/thiefList">Thief List</LinkButton>
-			<LinkButton to="/userList">User List</LinkButton>
+			<LinkButton to="/thiefList">Thiefs</LinkButton>
+			<LinkButton to="/userList">Users</LinkButton>
 			<LinkButton to="/import">Data</LinkButton>
 			<LinkButton to="/about">About</LinkButton>
 			<LinkButton to="/logout">Logout</LinkButton>
