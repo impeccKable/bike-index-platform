c5ae9ac609f3f93fab713bac072f07f105a47654
ec2-user <bike-index-platform@brucetruong.com>
2023-08-11 01:25:54 +0000

    http to https redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,6 +23,9 @@ export default function App() {
 		setDebug(true);
 		// setDev(true);
 		if (debug == true) { console.log("App"); }
+		if (window.location.protocol === 'http:') {
+			window.location.href = window.location.href.replace('http', 'https');
+		}
 	})
 
 	return (
