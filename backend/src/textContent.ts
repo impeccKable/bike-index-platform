231430544ca19c8c1ae9e847077c634beabf1293
ProceduralCode <37784785+ProceduralCode@users.noreply.github.com>
2023-08-11 00:29:00 -0700

    cleanup

--- backend/src/textContent.ts
+++ backend/src/textContent.ts
@@ -5,18 +5,18 @@ import db from "./dbConfig";
 const router = express.Router();
 
 /// Summary: makes a query to the text_content db table for a row with a matching pagename
-///       as the argument parameter. if a page a found the row is returned to the calling
-///       routine else if not row is found a new row is inserted then returned to the calling
-///       routine.
+///          as the argument parameter. if a page a found the row is returned to the calling
+///          routine else if not row is found a new row is inserted then returned to the calling
+///          routine.
 /// Params:
-///	 - "pageName": page name that text content belongs to.
+///     - "pageName": page name that text content belongs to.
 /// Returns: array of {contentid:"", page_name:"", label:"", body:"", isHidden:""}
-async function GetByPageName(pageName: string) {
+async function getByPageName(pageName: string) {
 	try {
 		let exists = await db.any("SELECT contentid FROM text_content WHERE page_name = ($1)", [pageName]);
 
-		if (exists.length === 0 && !IsNullOrEmpty(pageName)) {
-			let result = await db.any("INSERT INTO text_content (page_name, label, body, isHidden) VALUES ($1, $2, $3, $4);",[pageName, pageName, '', false]);
+		if (exists.length === 0 && pageName) {
+			await db.any("INSERT INTO text_content (page_name, label, body, isHidden) VALUES ($1, $2, $3, $4);",[pageName, pageName, '', false]);
 		}
 
 		let response = await db.any("SELECT contentid, page_name, label, body, ishidden FROM text_content WHERE page_name = ($1);", [pageName]);
@@ -27,36 +27,31 @@ async function GetByPageName(pageName: string) {
 	}
 }
 
-function IsNullOrEmpty(value: any) {
-	return value === null || value === undefined || value === "";
-}
-
 /// Summary: Updates a text content row in the database if it exists.
-///       Rows updated are those found in the object at run time.
-///       if the page does not exist in the table
+///          Rows updated are those found in the object at run time.
+///          if the page does not exist in the table
 /// Params:
-///	 - "data": Object of table attributes to update
+///     - "data": Object of table attributes to update
 /// Returns: No Return value.
-async function UpdatePageContent(data: any) {
+async function updatePageContent(data: any) {
 	try {
-		//let query = `SELECT contentid FROM text_content WHERE page_name = ($1)`;
 		let exists = await db.any("SELECT contentid FROM text_content WHERE page_name = ($1)", [data["pageName"]]);
-		if (exists.length === 0 || IsNullOrEmpty(data["pageName"])) {
+		if (exists.length === 0 || !data["pageName"]) {
 			console.log(`[ backend.src.textContent.ts.GetByPageName() ] Page '${data.pageName}' Not Found in text_content table. Update Not Executed.`);
 			return {body:"No Data Found"};
 		}
-        let sets: string[] = [];
-        Object.keys(data).forEach( (key) => {
-            if (key === 'pageName') return;
-            sets.push(`${key} = $(${key})`);
-        });
-        return await db.any(`
-            UPDATE text_content
-            SET ${sets.join(', ')}
-            WHERE page_name = $(pageName);
-        `, data);
+		let sets: string[] = [];
+		Object.keys(data).forEach( (key) => {
+			if (key === 'pageName') return;
+			sets.push(`${key} = $(${key})`);
+		});
+		return await db.any(`
+			UPDATE text_content
+			SET ${sets.join(', ')}
+			WHERE page_name = $(pageName);
+		`, data);
 
-    } catch (exc) {
+	} catch (exc) {
 		console.log(`[ backend.src.textContent.ts.UpdatePageContent() ] Error Attempting To Update Text Content: Exception Message: ${exc}`);
 		return {body:"No Data Found"};
 	}
@@ -64,7 +59,7 @@ async function UpdatePageContent(data: any) {
 
 router.get('/', async (req: express.Request, res: express.Response) => {
 	try {
-		let response = await GetByPageName(req.query.pageName?.toString() ?? "");
+		let response = await getByPageName(req.query.pageName?.toString() ?? "");
 		if (response.body !== "No Data Found") {
 			res.json(response);
 		} else {
@@ -77,7 +72,7 @@ router.get('/', async (req: express.Request, res: express.Response) => {
 
 router.put('/', async (req: express.Request, res: express.Response) => {
 	try {
-		const response = await UpdatePageContent(req.body);
+		const response = await updatePageContent(req.body);
 		// error status not working...
 		if (response.body !== "No Data Found") {
 			res.json("Success");
