5eab14ebfb8beba04bc249a80d192f2ef3bc639b
ec2-user <bike-index-platform@brucetruong.com>
2023-08-11 01:42:37 +0000

    Add run/stop scripts

---
+++ scripts/stop_frontend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pkill -f bike-index-platform/frontend
+
+# kills processes that match "bike-index-platform/frontend"
+#
+# pkill --- send signal to process based on their name or attritubes
+# -f --- option for matching anything after this flag
