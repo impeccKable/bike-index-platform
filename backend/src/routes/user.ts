0a7e52d2706692a24effb2738e548a54e4b9a7cf
Cristian Torres Salazar <tor8@pdx.edu>
2023-08-15 16:10:37 -0700

    Updated User.ts file post routes moved to own file

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import { getAllUsers, getUserByID, getUserBySearchType } from '../userData';
+import express from "express";
+import { GetAllUsers, GetUserByID, GetUserBySearchType } from "../userData";
 
 const router = express.Router();
 
@@ -8,14 +8,15 @@ router.get("/", async (req: express.Request, res: express.Response) => {
 		let key = req.query.searchKey;
 		let type = req.query.searchType;
 		if (!type || !key || key === "All") {
-			return res.json(await getAllUsers());
+			return res.json(await GetAllUsers());
 		}
-		return res.json(await getUserBySearchType(key.toString(), type.toString()));
+		return res.json(await GetUserBySearchType(key.toString(), type.toString()));
 	} catch (exc) {
 		console.log(`[ backend.src.user.ts.get('/') Error Attempting To Get All Users. Message: ${exc} ]`)
 	}
 });
 
+//example query: http://localhost:3000/api/user/userId=2oiwJmvUawY0RcdzQx5iE1ks7kC3
 router.get("/:userId", async (req: express.Request, res: express.Response) => {
 	try {
 		let userID = req.params.userId?.toString()?.replace("userId=", '') ?? '';
@@ -23,7 +24,7 @@ router.get("/:userId", async (req: express.Request, res: express.Response) => {
 		if (!userID) {
 			return {};
 		}
-		return res.json(await getUserByID(userID));
+		return res.json(await GetUserByID(userID));
 	} catch (exc) {
 		console.log(`[ backend.src.user.ts.get('/:userId') Error Attempting To Get All Users. Message: ${exc} ]`)
 	}
