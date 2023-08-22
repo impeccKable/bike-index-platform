c60a2c91f57d492a21a32dec136a3883202e508f
m-istvan <56458289+m-istvan@users.noreply.github.com>
2023-08-22 00:33:23 -0700

    userList redirect

--- frontend/src/pages/UserList.tsx
+++ frontend/src/pages/UserList.tsx
@@ -5,9 +5,11 @@ import LinkTable from "../components/LinkTable";
 import { FormInput, LinkButton, FormInputProps } from "../components/Form";
 import { httpClient } from "../services/HttpClient";
 import TextWindow from "../components/TextWindow";
+import { useAuth } from "../services/AuthProvider";
 
 // react
 import {useState, useEffect, useRef} from 'react';
+import { useNavigate } from "react-router-dom";
 
 
 const tableHeaders = {
@@ -60,6 +62,8 @@ export default function UserList() {
     const [searchType, setSearchType] = useState('name');
     const [searchText, setSearchText] = useState('');
     const [userData, setUserData] = useState<User[]>([]);
+    const { user } = useAuth();
+    const navigate = useNavigate();
     const preSearchText = useRef(searchText);
     const url = new URL(window.location.href);
 	const pageName = "User List";
@@ -67,11 +71,15 @@ export default function UserList() {
     SearchInputProps.value = searchText;
 
     useEffect(() => {
+        if(!user) return;
+        if(user?.bikeIndex.role !== "admin") {
+            navigate("/user/?userId=" + user.firebase.uid);            
+        }
         const type = url.searchParams.get("searchType");
         const text = url.searchParams.get("searchText");
         setSearchType(type ? type : "name");
         setSearchText(text ? text : "");
-    },[]);
+    },[user]);
 
     useEffect(() => {
         url.searchParams.set('searchType', searchType);
