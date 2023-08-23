368db4a118b6c1d065f5e117de29717342dc6dab
Cheng Lin <chlin@pdx.edu>
2023-08-22 17:59:41 -0700

    merge conflict

--- backend/src/routes/token.ts
+++ backend/src/routes/token.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { auth } from '../../app';
 
-export function validToken(req: express.Request):boolean {
+export function validToken(req: express.Request):string {
 	let token = '';
+	let uid = '';
 	if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
 		token = req.headers.authorization.split(' ')[1];
 		if (token[0] === `"`) {
@@ -10,23 +11,21 @@ export function validToken(req: express.Request):boolean {
 		}
 	}
 	if (!token) {
-		return false;
+		throw new Error("No token in header");
 	}
-	try {
-		auth.verifyIdToken(token);
-		return true;
-	} catch (err) {
-		return false;
-	}
+	auth.verifyIdToken(token)
+		.then((decodedToken:any)=>{uid=decodedToken.uid})
+		.catch((err:any)=>{throw new Error(err.message)});
+	return uid;
 }
 
 const router = express.Router();
 router.post("/", async (req: express.Request, res: express.Response) => {
 	try {
-		return res.json();
+		return res.json(validToken(req));
 	} catch (err) {
 		console.error(err);
-		res.status(500);
+		res.status(404);
 	}
 });
 export default router;
--- frontend/src/components/AuthProtected.tsx
+++ frontend/src/components/AuthProtected.tsx
@@ -2,15 +2,26 @@ import { useAuth } from "../services/AuthProvider";
 import { Navigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useNavigate } from "../../node_modules/react-router-dom/dist/index";
+import { httpClient } from "../services/HttpClient";
 
 export function AuthProtected({ children }: any) {
 	const { user, loading }: any = useAuth();
+	let response = null;
 
 	useEffect(() => {
-		console.log('user', user);
+		httpClient.post("/token").then((res) => {
+			response = res;
+			console.log(response);
+			//Possibly change to !==200
+			if (response.status === 404) {
+				<Navigate to="/logout" replace />;
+			}
+		}).catch((err: any) => { console.log(err); }
+		);
+		console.log(user);
 	}, [loading]);
 
-	if (!loading&&!user) {
+	if (!loading && !user) {
 		return <Navigate to="/" replace />;
 	} else if (loading) {
 		return <div>Loading...</div>;
